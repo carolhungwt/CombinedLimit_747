52e43e8e5301747ff548704cc7f3ff5baeab16eb
Wai Ting Hung <carolhungwt@gmail.com>
2018-02-09 23:27:14 +0100

    added vh option

--- signal_preparation/replaceReso.py
+++ signal_preparation/replaceReso.py
@@ -5,33 +5,39 @@ import argparse,os
 channels=['4e','2e2mu','4mu']
 files = ['clean_quad9_mH125','vbf_quad9_mH125','vh_quad9_mH125']
 
+def readparams(chan,ifile):
+  addtag = ''
+  if 'vh' in ifile:  addtag='_ZH'
+  resoloc = os.path.join(args.loc,'Reso_{chan}_quad9{addtag}.txt'.format(chan=chan,addtag=addtag))
+  resoloc = os.path.abspath(resoloc)
+  try:
+    with open(resoloc,'r') as f:
+      resostr = f.read()      
+    return resostr
+  except:
+    raise IOError("cannot locate "+resoloc)
+  
+
+
 if __name__=="__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-l','--loc',dest='loc',required=True)
 	parser.add_argument('-a','--arg')
 	args = parser.parse_args()
 	tempdict,cmdstr={},''
-	for chan in channels:
-		resoloc = os.path.join(args.loc,'Reso_'+chan+'_quad9.txt')
-		resoloc = os.path.abspath(resoloc)
-		try:
-			with open(resoloc,'r') as f:
-				resostr = f.read()	
-				tempdict[chan]=resostr
-		except: 
-			raise IOError('Cannot not find '+resoloc)
 	for ifile in files:
-		try:
-			with open(ifile+'_tmpl.c','r') as fin:
-				cfilestr = fin.read()
-				for key,item in tempdict.iteritems():
-					replstr = '<dcbPara_{chan}_2nd>'.format(chan=key)
-#					print replstr
-					cfilestr = cfilestr.replace(replstr,item)
-				with open(ifile+'.c','w') as fout:
-					fout.write(cfilestr)
-		except:
-			raise IOError('Cannot not find '+ifile+'_tmpl.c')
-		cmdstr+=ifile+'.c '
-	if args.arg:
-		os.system('mkdir -p '+args.arg+'; cp '+cmdstr+' '+args.arg)
+	  try:
+	    with open(ifile+'_tmpl.c','r') as fin:
+		cfilestr = fin.read()	
+		for chan in channels: 
+		  resostr = readparams(chan,ifile)
+		  replstr = '<dcbPara_{chan}_2nd>'.format(chan=chan)
+		  assert replstr in cfilestr 
+		  cfilestr = cfilestr.replace(replstr,resostr)
+		with open(ifile+'.c','w') as fout:
+		  fout.write(cfilestr)
+	  except:
+	    raise IOError('Cannot not find '+ifile+'_tmpl.c')
+	  cmdstr+=ifile+'.c '
+	  if args.arg:
+	    os.system('mkdir -p '+args.arg+'; cp '+cmdstr+' '+args.arg)
